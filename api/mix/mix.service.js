7eabaa6b7db4562636783fc366e31203725216dd
Eilon Shetrit <eilonshetrit@gmail.com>
2020-11-30 19:13:46 +0200

    fsd

--- api/mix/mix.service.js
+++ api/mix/mix.service.js
@@ -72,9 +72,9 @@ async function add(mix) {
 async function update(mix) {
     const collection = await dbService.getCollection('mix')
     mix._id = ObjectId(mix._id);
-
+    console.log('mixId', mix._id);
     try {
-        await collection.replaceOne({ _id: mix._id }, { $set: mix })
+        await collection.replaceOne({ '_id': mix._id }, { $set: mix })
         return mix
     } catch (err) {
         console.log(`ERROR: cannot update mix ${mix._id}`)
