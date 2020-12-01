b094bcff47ae59b1356620e5c05cb18adc019e34
Eilon Shetrit <eilonshetrit@gmail.com>
2020-12-01 11:41:10 +0200

    dasdasdas

--- server.js
+++ server.js
@@ -48,7 +48,6 @@ if (process.env.NODE_ENV === 'production') {
  
 const authRoutes = require('./api/auth/auth.routes')
 const userRoutes = require('./api/user/user.routes')
-const reviewRoutes = require('./api/review/review.routes')
 const mixRoutes = require('./api/mix/mix.routes')
 const connectSockets = require('./api/socket/socket.routes')
      
@@ -56,7 +55,6 @@ const connectSockets = require('./api/socket/socket.routes')
 // routes
 app.use('/api/auth', authRoutes)
 app.use('/api/user', userRoutes)
-app.use('/api/review', reviewRoutes)
 app.use('/api/mix', mixRoutes)
 connectSockets(io)
  
